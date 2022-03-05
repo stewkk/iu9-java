cbb81d8202def3bfa331446948bddff3cf0dd2a1
Starovoytov Alexandr <saniy200311@gmail.com>
2022-03-05 23:13:58 +0300

    Траектория движения земли

--- 2_2/Test.java
+++ 2_2/Test.java
@@ -1,31 +1,12 @@
 public class Test {
     public static void main(String[] args) {
-        // Universe universe = new Universe(0);
-        // universe.add(1, 2, 0, 0, 10, 0, 1e5);
-        // universe.add(4, 3, 0, 0, 0, 0, 3e12);
-        // universe.dump("plot.dat", false);
-        // for (int i = 0; i < 100000; ++i) {
-        //     universe.recalcCoords(0.01);
-        //     universe.dump("plot.dat", true);
-        // }
         Universe universe = new Universe(0);
-        universe.add(3, 3, 0, 0, 14, 4, 1e3);
-        universe.add(4, 3, 0, 0, 0, 0, 3e12);
-        universe.add(4, 40, 0, 0, 0, 0, 4e12);
-        universe.add(4, 80, 0, 0, 0, 0, 3e12);
-        universe.add(4, 120, 0, 0, 0, 0, 4e12);
-        universe.add(4, 160, 0, 0, 0, 0, 3e12);
-        universe.add(4, 200, 0, 0, 0, 0, 4e12);
+        universe.add(0, 0, 0, 0, 0, 0, 1.989E30); // солнце
+        universe.add(147098290e3, 0, 0, 0, 30270, 0, 5.972e24); // земля
         universe.dump("plot.dat", false);
-        for (int i = 0; i < 1500; ++i) {
-            universe.recalcCoords(0.01);
+        for (int i = 0; i < 1*24*365; ++i) { // год
+            universe.recalcCoords(60*60); // час
             universe.dump("plot.dat", true);
         }
-        // Universe universe = new Universe(10);
-        // universe.dump("plot.dat", false);
-        // for (int i = 0; i < 100; ++i) {
-        //     universe.recalcCoords(0.001);
-        //     universe.dump("plot.dat", true);
-        // }
     }
 };
--- 2_2/test.plt
+++ 2_2/test.plt
@@ -1,8 +1,9 @@
 #!/usr/bin/env -S gnuplot
 
+set terminal postscript
+set output '| ps2pdf - output.pdf'
 set key off
 file='plot.dat'
 stats file nooutput
-splot for [i=1:STATS_columns:3] 'plot.dat' using i:i+1:i+2 with linesp ls 2
-# plot 'plot.dat' using 1:2 with dots dt 2 lw 4, 'plot.dat' using 4:5 with dots dt 2 lw 4
-pause -1
+splot 'plot.dat' using 1:2:3 with dots dt 2, for [i=4:STATS_columns:3] 'plot.dat' using i:i+1:i+2 with lines ls 1
+# pause -1
